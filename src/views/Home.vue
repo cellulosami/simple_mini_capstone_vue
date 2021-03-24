a3264d503d16169bf0e28a3d7bb23861343f5bd8
austin shields <ajshields196@gmail.com>
2021-03-23 21:21:18 -0400

    add products index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
+/* eslint-disable prettier/prettier */
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      {{ product.name }}
+      <div>
+        <img v-bind:src="product.image_url">
+      </div>
+      <hr>
+      </div>
   </div>
 </template>
 
+<style>
+img {
+  width: 250px;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+    };
+  },
+  created: function () {
+    console.log("hello");
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      console.log("hello from indexProducts");
+      axios.get("http://localhost:3000/api/products").then((response) => {
+        console.log(response);
+        this.products = response.data;
+      });
+    },
   },
 };
 </script>
